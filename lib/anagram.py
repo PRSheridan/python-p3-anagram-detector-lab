9802e5d3c54f59e716539aca02d55fc6ae36d0e2
Phil <philrsheridan@gmail.com>
2024-03-28 16:27:14 -0400

    passing all tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, input_list):
+        answers = []
+
+        for input in input_list:
+            control = sorted([letter for letter in self.word])
+            test = sorted([letter for letter in input])
+            if control == test:
+                answers += [input]
+                
+        return answers
